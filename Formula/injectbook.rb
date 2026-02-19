88b52d1007a113432e6874132aee24079214b6c2
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2026-02-19 23:26:11 +0530

    release: sync formula SHA256 for v0.4.0

--- Formula/injectbook.rb
+++ Formula/injectbook.rb
@@ -7,10 +7,10 @@ class Injectbook < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/prashantbhudwal/injectbook/releases/download/v0.4.0/injectbook-v0.4.0-darwin-arm64.tar.gz"
-      sha256 "REPLACE_WITH_ARM64_SHA256"
+      sha256 "51e5173701b65b73bcc14ac5bf530e90d34574e9a6eaf7b3e6c5420b8342fcc6"
     else
       url "https://github.com/prashantbhudwal/injectbook/releases/download/v0.4.0/injectbook-v0.4.0-darwin-amd64.tar.gz"
-      sha256 "REPLACE_WITH_AMD64_SHA256"
+      sha256 "641e6f942bae2a29fd92c814d84580fb04fae3ce5527796dffa72bd86d05620a"
     end
   end
 
